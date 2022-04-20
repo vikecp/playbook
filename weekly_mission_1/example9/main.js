f3319a1b3c627857132801620f1170ed2a26f73a
VirginiaCastaneda <vikeyazull@gmail.com>
2022-04-20 00:25:07 -0500

    finished exercise 9 i'm happy

---
+++ weekly_mission_1/example9/main.js
@@ -0,0 +1,19 @@
+//const Pokemon = require('./pokemon')
+import Pokemon from './pokemon.js'
+
+const pikachu = new Pokemon("pikachu")
+const bulbasaur = new Pokemon("bulbasaur")
+const squirtle = new Pokemon("squirtle")
+const charmander = new Pokemon("charmander")
+
+pikachu.sayHello()
+pikachu.sayMessage("Heey!")
+
+bulbasaur.sayHello()
+bulbasaur.sayMessage("Heey!")
+
+charmander.sayHello()
+charmander.sayMessage("Heey!")
+
+squirtle.sayHello()
+squirtle.sayMessage("Heey!")
