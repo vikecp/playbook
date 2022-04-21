1cffa6dc08aecf5a4f96b1861fc0829d65736c16
VirginiaCastaneda <vikeyazull@gmail.com>
2022-04-20 20:02:16 -0500

    1.Agregando clase y archivo index.js

---
+++ weekly_mission_3/projects/my_first_project/app/ajolonauta.js
@@ -0,0 +1,8 @@
+class Ajolonauta {
+
+    constructor(name){ // para instanciar los ejemplos
+        this.name =  name
+    }
+}
+
+module.exports = Ajolonauta; // exportar modulo
---
+++ weekly_mission_3/projects/my_first_project/index.js
@@ -0,0 +1,3 @@
+const Ajolonauta = require('./app/ajolonauta')
+const woopa = new  Ajolonauta("Woopa")
+console.log(woopa.name)
