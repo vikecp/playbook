11370adbd2de89aecd536aada6b7df123a362fca
VirginiaCastaneda <vikeyazull@gmail.com>
2022-04-21 00:12:01 -0500

    6. Se agrego index.js

---
+++ weekly_mission_3/projects/1_my_launchx_app/index.js
@@ -0,0 +1,7 @@
+const Commander = require('./app/missionCommander');
+const carlo = new Commander("Carlo")
+const fer = new Commander("Fernanda")
+const rod = new Commander("Rodrigo")
+console.log(carlo.name)
+console.log(fer.name)
+console.log(rod.name)
