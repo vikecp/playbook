0899280882f6b24437a2c9a85d9a44741ab43d78
VirginiaCastaneda <vikeyazull@gmail.com>
2022-05-03 17:33:55 -0500

    Twitter Paso 8: Spring 2 Req 2 crear metodo static getInfo que recibe un objeto y regresa una lista de todos los valores del objeto

--- weekly_mission_3/Practicas/03_practica/twitter/app/services/userService.js
+++ weekly_mission_3/Practicas/03_practica/twitter/app/services/userService.js
@@ -4,5 +4,9 @@ class UserService{
     static create(id, username, name){
         return new User(id, username, name,"Sin bio")
     }
+
+    static getInfo(user){
+        return Object.values(user)
+    }
 }
 module.exports = UserService
--- weekly_mission_3/Practicas/03_practica/twitter/test/services/UserService.test.js
+++ weekly_mission_3/Practicas/03_practica/twitter/test/services/UserService.test.js
@@ -8,5 +8,13 @@ describe('Test for userService', ()=> {
         expect(user.user).toBe("viike")
         expect(user.name).toBe("Virginia")
         expect(user.bio).not.toBeUndefined()
+    });
+    test('2. Get all data user in a list', ()=>{
+        const user = UserService.create(1, "viike","Virginia")
+        const userInfoList = UserService.getInfo(user)
+        expect(userInfoList[0]).toBe(1)
+        expect(userInfoList[1]).toBe("viike")
+        expect(userInfoList[2]).toBe("Virginia")
+        expect(userInfoList[3]).toBe("Sin bio")
     })
 })
