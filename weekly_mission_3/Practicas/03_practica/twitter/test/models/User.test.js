90cbe15797821528c294cdd4f10651c850506765
VirginiaCastaneda <vikeyazull@gmail.com>
2022-05-03 13:43:52 -0500

    Twitter Paso 4: Req 2 Las propiedades dateCreated y lastUpdated

--- weekly_mission_3/Practicas/03_practica/twitter/test/models/User.test.js
+++ weekly_mission_3/Practicas/03_practica/twitter/test/models/User.test.js
@@ -3,13 +3,13 @@ const User = require('./../../app/models/User');
 describe("Unit test for class user", () => {
     test("Create an user object", () =>{
         //invocar el codigo que se va a usar
-        const user = new User(1, "viikecp", "Virginia", "Bio", "DateCreated", "last");
+        const user = new User(1, "viikecp", "Virginia", "Bio");
         //lo esperado
         expect(user.id).toBe(1)
         expect(user.user).toBe("viikecp")
         expect(user.name).toBe("Virginia")
         expect(user.bio).toBe("Bio")
-        expect(user.dateCreated).toBe("DateCreated")
-        expect(user.lastUpdated).toBe("last")
+        expect(user.dateCreated).not.toBeUndefined()
+        expect(user.lastUpdated).not.toBeUndefined()
     })
 })
